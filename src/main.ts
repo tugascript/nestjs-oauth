9bfb388e0e24ebe938a3c14ea18aa9f657706a23
Afonso Barracha <barracha.afonso@gmail.com>
2023-01-15 19:00:03 +0000

    chore: add cors

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,23 @@
 */
 
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  app.enableCors({
+    credentials: true,
+    origin: `https://${configService.get<string>('domain')}`,
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+  await app.listen(configService.get<number>('port'));
 }
 
 bootstrap();
