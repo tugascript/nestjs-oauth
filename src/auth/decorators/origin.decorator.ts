aab17ee192d8680abcd3ede4bb0e5394e85d7f04
Afonso Barracha <barracha.afonso@gmail.com>
2023-01-20 12:26:05 +0000

    fix(auth): fix origin decorator

--- src/auth/decorators/origin.decorator.ts
+++ src/auth/decorators/origin.decorator.ts
@@ -5,9 +5,10 @@
 */
 
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { Request } from 'express';
 
 export const Origin = createParamDecorator(
   (_, context: ExecutionContext): string | undefined => {
-    return context.switchToHttp().getRequest()?.origin;
+    return context.switchToHttp().getRequest<Request>().headers?.origin;
   },
 );
