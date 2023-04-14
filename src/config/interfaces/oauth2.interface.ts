f4703a6b20d3ef6ec2a4bd765973272c63e739e7
Afonso Barracha <barracha.afonso@gmail.com>
2023-04-14 16:09:08 +0100

    chore: run yarn lint and format

--- src/config/interfaces/oauth2.interface.ts
+++ src/config/interfaces/oauth2.interface.ts
@@ -1,16 +1,7 @@
 /*
- Free and Open Source - GNU GPLv3
-
- This file is part of nestjs-graphql-fastify-template
-
- nestjs-graphql-fastify-template is distributed in the
- hope that it will be useful, but WITHOUT ANY WARRANTY;
- without even the implied warranty of MERCHANTABILITY
- or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- General Public License for more details.
-
- Copyright © 2023
- Afonso Barracha
+  Free and Open Source - GNU LGPLv3
+  Copyright © 2023
+  Afonso Barracha
 */
 
 import { IClient } from '../../oauth2/interfaces/client.interface';
